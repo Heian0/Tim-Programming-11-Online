5714d71570bd2ceb65a5d63d2b585faab6ca2e30
Heian0 <77514857+Heian0@users.noreply.github.com>
2022-07-06 17:40:47 -0700

    Improving the FriendsBook

--- friends/src/main/java/com/example/friends/HelloController.java
+++ friends/src/main/java/com/example/friends/HelloController.java
@@ -1,13 +1,14 @@
 package com.example.friends;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class HelloController
@@ -23,6 +24,55 @@ public class HelloController
     public Label ageLabel;
 
     public Friend selectedFriend;
+    public ChoiceBox saveToChoice;
+    public ChoiceBox loadFromChoice;
+
+    ObservableList<String> fileList = FXCollections.observableArrayList("schoolFriends.txt", "workFriends.txt");
+
+    @FXML
+    public void initialize()
+    {
+        saveToChoice.setValue("schoolFriends.txt");
+        saveToChoice.setItems(fileList);
+        loadFromChoice.setValue("schoolFriends.txt");
+        loadFromChoice.setItems(fileList);
+    }
+
+    public void SaveFriends(ActionEvent actionEvent) throws IOException {
+
+        FileWriter fw = new FileWriter("src/" + saveToChoice.getValue(), true);
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        ObservableList<Friend> list = friendsList.getItems();
+        for (int i=0; i<list.size(); i++)
+        {
+            String friendStr = list.get(i).GetName() + " " + String.valueOf(list.get(i).GetPhone()) + " " + String.valueOf(list.get(i).GetAge());
+            bw.write("\n" + friendStr);
+            System.out.println(friendStr);
+        }
+        bw.close();
+    }
+
+    public void LoadFriends(ActionEvent actionEvent) throws IOException {
+        FileInputStream fstream = new FileInputStream("src/" + loadFromChoice.getValue());
+        DataInputStream in = new DataInputStream(fstream);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String line;
+        while ((line = br.readLine()) != null)
+        {
+            String[] split = line.split(" ");
+
+
+            Friend temp = new Friend( split[0], Long.parseLong(split[1]), Integer.parseInt(split[2]) );
+
+            friendsList.getItems().add(temp);
+
+            getNameText.clear();
+            getPhoneText.clear();
+            getAgeText.clear();
+        }
+
+    }
 
     public void NewFriendClicked(ActionEvent actionEvent)
     {
